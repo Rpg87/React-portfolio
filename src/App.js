c82628f1848f079928253730d536106d35716dc1
Rpg87 <sra.picadillo@gmail.com>
2023-10-27 10:51:47 +0200

    m

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Route
           path="/"
           element={<Layout />}
+
         >
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
